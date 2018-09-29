07a489e7e61da2ca033ff5c785421c12427adfd1
Sean Basler <Holoimageer13@gmail.com>
2018-09-29 14:56:05 -0500

    Camera with spring like attach

--- Assets/Scripts/Cammera_Controller.cs
+++ Assets/Scripts/Cammera_Controller.cs
@@ -8,7 +8,15 @@ public class Cammera_Controller : MonoBehaviour {
     [SerializeField]
     private Transform followObject;
 
+    [SerializeField]
+    private float deltaSpeed = 0.5f;
+
+    [SerializeField]
+    private float smoothTime = 0.25f;
+
     private Vector3 moveTemp;
+    private float tempDeltaX;
+    private float moveSpeed;
 
     [SerializeField]
     private Vector3 offset = new Vector3(5.0f, 0.0f, 0.0f);
@@ -20,12 +28,31 @@ public class Cammera_Controller : MonoBehaviour {
 
     void LateUpdate()
     {
-        moveTemp = followObject.transform.position;
-        moveTemp.x += offset.x;
-        moveTemp.y = offset.y;
-        moveTemp.z = transform.position.z;
-        transform.position = moveTemp;
+        if (Input.GetAxis("Horizontal") > 0.0)
+        {
+            moveSpeed += deltaSpeed;
+        }
+        else if (moveSpeed > 0)
+        {
+            moveSpeed -= deltaSpeed;
+        }
+        else
+        {
+            moveSpeed = 0.0f;
+        }
 
+        print(moveSpeed);
+
+        moveTemp = followObject.transform.position;
+        if (moveSpeed > 5.0f)
+            moveTemp.x += offset.x;
+        else
+            moveTemp.x = moveTemp.x;
+        moveTemp.y = transform.position.y;
+        moveTemp.z = transform.position.z;
+        tempDeltaX = Mathf.SmoothDamp(transform.position.x, moveTemp.x, ref moveSpeed, smoothTime);
+        moveTemp.x = tempDeltaX;
+        transform.position = moveTemp;
     }
 	
 }
