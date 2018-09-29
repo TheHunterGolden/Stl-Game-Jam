61d619c3ede96c7175cac4cb9b14d84372ef675e
Timmy Gerlach <43688771+TheTimmyGerlach@users.noreply.github.com>
2018-09-29 03:03:23 -0500

    Basic ball movement
    Ball moves from point to point, provided it's linked to a RailPoint to start, and all RailPoints have a RailPoint they're linked to. This is not an ideal setup (doesn't account for pauses, switches, etc) but it counts as a tech demo.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Ball : MonoBehaviour {
+
+	[SerializeField]
+	private RailPoint targetRailPoint;
+	[SerializeField]
+	private Vector3 dir;
+	[SerializeField]
+	private float speed;
+
+	// Use this for initialization
+	void Start () {
+		getNextPoint();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate() {
+		transform.Translate(dir * speed);
+		if((targetRailPoint.transform.position - transform.position).sqrMagnitude < (dir * speed).sqrMagnitude) {
+			getNextPoint();
+		}
+	}
+
+	private void getNextPoint()
+	{
+		targetRailPoint = targetRailPoint.NextPoint;
+		dir = targetRailPoint.transform.position - transform.position ;
+		dir.Normalize();
+	}
+}
---
+++ Assets/Scripts/RailPoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class RailPoint : MonoBehaviour {
+
+	[SerializeField]
+	private RailPoint nextPoint;
+	public RailPoint NextPoint { get {return nextPoint; } }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
