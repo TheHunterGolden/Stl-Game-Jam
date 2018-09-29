9b28a6046c3ba78dd4065c30c1c368c102ed499f
Sean Basler <Holoimageer13@gmail.com>
2018-09-29 01:48:48 -0500

    Camera Controller Script

---
+++ Assets/Scripts/Cammera_Controller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cammera_Controller : MonoBehaviour {
+
+    private Vector2 velocity;
+
+    public GameObject player;
+    public float smoothTimeY;
+    public float smoothTimeX;
+
+    void LateUpdate()
+    {
+        float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
+        float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeX);
+
+        transform.position = new Vector3(posX, posY, transform.position.z);
+    }
+	
+}
