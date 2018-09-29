a041ffeae2f7b4abb6bb8d0c6f67906b9b0e8cd3
Timmy Gerlach <43688771+TheTimmyGerlach@users.noreply.github.com>
2018-09-29 14:01:53 -0500

    Listeners
    Abstract classes

---
+++ Assets/Scripts/ScriptableObjects/GameEvent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class GameEvent : ScriptableObject {
+
+	protected List<GameEventListener> listeners = new List<GameEventListener>();
+
+	public void RegisterListener(GameEventListener listener) {
+		listeners.Add(listener);
+	}
+
+	public void DeregisterListener(GameEventListener listener) {
+		listeners.Remove(listener);
+	}
+
+	public abstract void Raise() ;
+
+}
