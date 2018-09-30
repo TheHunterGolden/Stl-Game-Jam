b35974bd0eca00bfe906ee77228f0012ed7b0c35
TheHunterGolden <huntergolden0124@gmail.com>
2018-09-30 15:05:33 -0500

    newest scene

---
+++ Assets/DampenSpeed.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DampenSpeed : MonoBehaviour {
+
+    public GameObject sparky;
+    public float scale;
+    public float counter = 1;
+	// Use this for initialization
+	void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        counter =- Time.deltaTime;
+        if (counter > 0) {
+            counter = 1;
+            subtractSpeed(scale);
+        }
+    }
+
+    public void subtractSpeed(float s)
+    {
+    
+        sparky.GetComponent<Ball>().setSpeed(sparky.GetComponent<Ball>().getSpeed() - s);
+    }
+
+    public void resetSpeed() {
+
+    }
+}
